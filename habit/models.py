09465e83471f8aca7ff990b840d6ff7bab1674d6
Emily Starr <emilyflo.starr@gmail.com>
2022-03-22 13:41:24 -0400

    getting all set up and about to deploy to Heroku

---
+++ habit/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+# class Habit(models.Model):
+#     name=
+#     target=
+#     start=
+#     def __str__(self):
+#         return self.name
+
+# class Record(models.Model):
+#     habit=
+#     date=
+#     record=
+#     def __str__(self):
+#         return self.habit
